d5fc55e3c0d2d271e4f35010fea534e10fd6c2b3
Azzam Jihad Ulhaq <azzam.jiul@gmail.com>
2020-08-19 19:05:36 +0700

    first commit

---
+++ app/src/main/java/com/example/myitsinfo/MainActivity.kt
@@ -0,0 +1,60 @@
+package com.example.myitsinfo
+
+import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+//        setSupportActionBar(findViewById(R.id.toolbar))
+
+        // Kode Webview Start
+        val myWebView : WebView = findViewById(R.id.webKu)
+
+        myWebView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(
+                    view: WebView?,
+                    url: String?
+            ): Boolean {
+                if (url != null) {
+                    view?.loadUrl(url)
+                }
+                return true
+            }
+        }
+
+        myWebView.loadUrl("https://www.its.ac.id/ppid/")
+        myWebView.settings.javaScriptEnabled=true
+        myWebView.settings.allowContentAccess=true
+        myWebView.settings.domStorageEnabled=true
+        myWebView.settings.useWideViewPort=true
+        myWebView.settings.setAppCacheEnabled(true)
+
+        // Kode WebView End
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
